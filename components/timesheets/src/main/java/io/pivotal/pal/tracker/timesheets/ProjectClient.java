2b427b6acabddbac47d27d347d3e16b7b5d6f674
David Edwards <dedwards@pivotal.io>
2017-07-19 15:21:41 -0600

    Add circuit breakers

--- components/timesheets/src/main/java/io/pivotal/pal/tracker/timesheets/ProjectClient.java
+++ components/timesheets/src/main/java/io/pivotal/pal/tracker/timesheets/ProjectClient.java
@@ -1,18 +1,34 @@
 package io.pivotal.pal.tracker.timesheets;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.client.RestOperations;
 
-public class ProjectClient {
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
+public class ProjectClient {
     private final RestOperations restOperations;
-    private final String endpoint;
+    private final String registrationServerEndpoint;
+    private ConcurrentMap<Long, ProjectInfo> cachedProjects = new ConcurrentHashMap<>();
 
     public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
-        this.restOperations = restOperations;
-        this.endpoint = registrationServerEndpoint;
+        this.restOperations= restOperations;
+        this.registrationServerEndpoint = registrationServerEndpoint;
     }
 
+    @HystrixCommand(fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
-        return restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+        ProjectInfo fetchedProjectInfo = restOperations
+                .getForObject(
+                        registrationServerEndpoint + "/projects/" + projectId,
+                        ProjectInfo.class
+                );
+
+        cachedProjects.put(projectId, fetchedProjectInfo);
+        return fetchedProjectInfo;
+    }
+
+    public ProjectInfo getProjectFromCache(long projectId) {
+        return cachedProjects.get(projectId);
     }
 }
